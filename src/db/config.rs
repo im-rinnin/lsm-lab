723b35797f2632f2e0e5ab7b32e75cde08d03394
patchoulig <378844395@qq.com>
2023-01-02 21:58:54 +0800

    add metric

--- src/db/config.rs
+++ src/db/config.rs
@@ -1,8 +1,12 @@
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 pub struct Config {
     pub sstable_file_limit: usize,
     pub level_0_file_limit: usize,
     pub level_size_expand_factor: usize,
+    pub meta_log_file_name: String,
+    pub sstable_meta_cache: usize,
+    pub memtable_size_limit: usize,
+    pub level_0_len_to_slow_write_threshold: usize,
 }
 
 impl Config {
@@ -11,6 +15,10 @@ impl Config {
             sstable_file_limit: 2 * 1024 * 1024,
             level_0_file_limit: 4,
             level_size_expand_factor: 10,
+            meta_log_file_name: String::from("meta"),
+            sstable_meta_cache: 100,
+            memtable_size_limit: 2 * 1024 * 1024,
+            level_0_len_to_slow_write_threshold: 4,
         }
     }
 }
